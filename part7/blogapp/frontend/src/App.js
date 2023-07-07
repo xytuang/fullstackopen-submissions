232d5b532676a87bdb1f76f7ec6d09f8ac921cea
Xiang Yu Tuang <xiangyutuang@gmail.com>
2023-07-07 13:25:50 +0800

    bloglist part 7.17

--- part7/blogapp/frontend/src/App.js
+++ part7/blogapp/frontend/src/App.js
@@ -15,8 +15,8 @@ import { initializeBlogs } from './reducers/blogReducer'
 import { initializeUser, logout } from './reducers/authReducer'
 import { initializeAllUsers } from './reducers/userReducer'
 
-import { Routes, Route } from 'react-router-dom'
-
+import { Routes, Route, Link } from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap'
 
 
 
@@ -44,11 +44,37 @@ const App = () => {
     )
   }
 
+  const padding = {
+    padding: 5
+  }
+
   return (
     <div>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/">home</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/blogs">blogs</Link>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              <Link style={padding} to="/users">users</Link>
+              <button onClick={handleLogout}>logout</button>
+            </Nav.Link>
+            <Nav.Link href="#" as="span">
+              {user
+                ? <em style={padding}>{user.name} logged in</em>
+                : <Link style={padding} to="/login">login</Link>
+              }
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
       <Notification/>
       <h2>blogs</h2>
-      <p>{user.name} logged in <button onClick={handleLogout}>logout</button></p>
       <Togglable buttonShow='new blog'  buttonHide='cancel'>
         <BlogForm/>
       </Togglable>
