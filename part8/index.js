bc1c8a0004d8b0b4a6bdecdf1684af37de0cd210
Xiang Yu Tuang <xiangyutuang@gmail.com>
2023-07-07 19:47:46 +0800

    library part8.5

--- part8/index.js
+++ part8/index.js
@@ -108,7 +108,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]
   }
 `
@@ -123,10 +123,19 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author){
+      if (!args.author && !args.genre){
         return books
+      }
+      if (args.author && args.genre){
+        return books.filter(b => b.author === args.author).filter(a => a.genres.includes(args.genre))
+      }
+      if (args.author){
+        return books.filter(b => b.author === args.author)
+      }
+      if (args.genre){
+        return books.filter(b => b.genres.includes(args.genre))
       }  
-      return books.filter(b => b.author === args.author)
+      
     },
     allAuthors: () => authors
   }
