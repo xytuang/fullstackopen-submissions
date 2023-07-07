e6f779d4128d239fc94814ec95e4f87ff2a3f313
Xiang Yu Tuang <xiangyutuang@gmail.com>
2023-07-07 19:42:43 +0800

    library part8.4

--- part8/index.js
+++ part8/index.js
@@ -108,7 +108,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]
   }
 `
@@ -122,7 +122,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      if (!args.author){
+        return books
+      }  
+      return books.filter(b => b.author === args.author)
+    },
     allAuthors: () => authors
   }
 }
